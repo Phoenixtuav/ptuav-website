e7bc6b4f5b5fc7d3ae471e2d07ecb4afde1abb04
YingLuoNou <YingLuoNou@users.noreply.github.com>
2026-01-28 14:29:41 +0800

    add something

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -1,18 +1,18 @@
 export const CONTACT_CONFIG = [
   {
-    platform: "GitHub",
-    address: "https://github.com/yingluonou",
+    platform: "公司地址",
+    address: "浙江省宁波市海曙区文化路88号D座3楼311室",
     // 这里使用简单的 SVG 路径字符串，或者你可以留空
-    svg: `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>`
+    svg: ``
   },
   {
     platform: "Email",
-    address: "mailto:contact@astrotech.com",
+    address: "mailto:francis_shaohuali@163.com",
     svg: `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>`
   },
   {
-    platform: "手机",
-    address: "",
+    platform: "客服电话",
+    address: "18835020985",
     svg: `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5"><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path><path d="M3 7l9 6l9 -6"></path></svg>`
   }
 ];
