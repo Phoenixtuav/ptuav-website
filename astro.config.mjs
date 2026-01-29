b871a73195d574b44c49e2fcc6bf4a0aa0b8cd7f
YingLuoNou <YingLuoNou@users.noreply.github.com>
2026-01-29 13:32:26 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import edgeoneAdapter from "@edgeone/astro";
 import tailwind from "@astrojs/tailwind";
 
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.plumesky.com',
   adapter: edgeoneAdapter(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
